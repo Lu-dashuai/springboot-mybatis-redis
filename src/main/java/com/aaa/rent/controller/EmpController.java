6a976ec74aaa58979992a46ba66af12336fdcf6d
sengedateformat <605480673@qq.com>
2018-11-29 11:20:04 +0800

    sengedateformat

--- src/main/java/com/aaa/rent/controller/EmpController.java
+++ src/main/java/com/aaa/rent/controller/EmpController.java
@@ -1,10 +1,20 @@
 package com.aaa.rent.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * className:EmpController
  * discription:
  * author:luRuiHua
  * createTime:2018-11-29 09:34
  */
+@RestController
 public class EmpController {
+
+    @RequestMapping("/hello")
+    public Object getList() {
+        return "hello world!";
+    }
+
 }
