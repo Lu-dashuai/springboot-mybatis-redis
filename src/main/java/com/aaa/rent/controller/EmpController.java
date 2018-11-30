30b38038f155ea92af493fc740949db5c2239fb3
芦瑞华 <aluruihua@163.com>
2018-11-30 13:58:01 +0800

    增加了测试empList，老子写了一套测试，说明框架绝对没问题，我已经导入springboot和thymleaf的整合的依赖。

--- src/main/java/com/aaa/rent/controller/EmpController.java
+++ src/main/java/com/aaa/rent/controller/EmpController.java
@@ -1,7 +1,14 @@
 package com.aaa.rent.controller;
 
+import com.aaa.rent.entity.Emp;
+import com.aaa.rent.service.EmpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * className:EmpController
@@ -9,12 +16,31 @@ import org.springframework.web.bind.annotation.RestController;
  * author:luRuiHua
  * createTime:2018-11-29 09:34
  */
-@RestController
-public class EmpController {
 
+@Controller
+@RequestMapping("/emp")
+public class EmpController {
+    @Autowired
+    private EmpService empService;
+    /**
+     * 测试跳转页面
+     * @return
+     */
     @RequestMapping("/hello")
-    public Object getList() {
+    @ResponseBody
+    public Object hello() {
         return "hello world!";
+    }
+
+    /**
+     * 查询emp,跳转empList页面
+     * @return
+     */
+    @RequestMapping("/empList")
+    public String empList(Model model) {
+        List<Emp> empList = empService.selectEmpList();
+        model.addAttribute(empList);
+        return "empList";
     }
 
 }
--- src/main/java/com/aaa/rent/dao/EmpDao.java
+++ src/main/java/com/aaa/rent/dao/EmpDao.java
@@ -1,5 +1,9 @@
 package com.aaa.rent.dao;
 
+import com.aaa.rent.entity.Emp;
+
+import java.util.List;
+
 /**
  * className:EmpDao
  * discription:
@@ -7,4 +11,9 @@ package com.aaa.rent.dao;
  * createTime:2018-11-29 09:54
  */
 public interface EmpDao {
+    /**
+     * 查询员工
+     * @return
+     */
+    public List<Emp> selectEmpList();
 }
